9a4b968816aa5aec32582e05b07bb3571e0c9edb
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-08-13 13:20:06 +0530

    Model created & CRUD Functions created

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const app = express();
 
 const port = 5000;
 
+const Product = require('../backend/Models/product'); // Make sure the path is correct
+
+
 //Creating the connection
 
 // Replace <username>, <password>, and <your-cluster-url> with your actual Atlas details
@@ -22,41 +25,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB Atlas Gayu"))
   .catch((err) => console.error("Could not connect to MongoDB Atlas...", err));
 
+
+//To check the server is runnig or not
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(express.json());
 app.use(cors());
 
 //Create the new product
 app.post("/create", async (req, res) => {
-  const newProduct = new Product({
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    discountPercentage: req.body.discountPercentage,
-    rating: req.body.rating,
-    stock: req.body.stock,
-    brand: req.body.brand,
-    category: req.body.category,
-    thumbnail: req.body.thumbnail,
-    images: req.body.images,
-  });
-
-  await Product.create(newProduct);
-  res.send("Product saved to the database!");
-});
-
-//Get the all product list
-app.get("/read", async (req, res) => {
-  const productList = await Product.find();
-  res.send(JSON.stringify(productList));
-});
-
-//Update a product based on the id
-app.put("/update/:id", async (req, res) => {
-    const product_id = req.params.id;
-    await Product.findByIdAndUpdate(product_id, {
+    const newProduct = new Product({
       title: req.body.title,
       description: req.body.description,
       price: req.body.price,
@@ -69,17 +49,11 @@ app.put("/update/:id", async (req, res) => {
       images: req.body.images,
     });
   
-    res.send("Product updated successfully!");
+    await Product.create(newProduct);
+    res.send("Product saved to the database!");
   });
 
-  //Delete a product based on the id
-app.delete("/delete/:id", async (req, res) => {
-    const product_id = req.params.id;
-    await Product.findByIdAndDelete(product_id);
-    res.send("Product deleted!");
-  });
 
-  
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
