4b2f1022662330792de8cf56ae944967df2c5f4c
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-08-13 12:38:19 +0530

    Model created & CRUD Functions created

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const cors = require("cors");
 
-
 //Importing the mongoose package
 
 const mongoose = require("mongoose");
@@ -14,7 +13,8 @@ const port = 5000;
 //Creating the connection
 
 // Replace <username>, <password>, and <your-cluster-url> with your actual Atlas details
-const mongoURI = "mongodb+srv://gayathirijeyananthan:gayu20010408@cluster0.1ys7fn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI =
+  "mongodb+srv://gayathirijeyananthan:gayu20010408@cluster0.1ys7fn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Creating the connection to MongoDB Atlas
 mongoose
@@ -22,18 +22,64 @@ mongoose
   .then(() => console.log("Connected to MongoDB Atlas Gayu"))
   .catch((err) => console.error("Could not connect to MongoDB Atlas...", err));
 
-
 app.get("/", (req, res) => {
-
-res.send("Hello World!");
-
+  res.send("Hello World!");
 });
 
-app.use(cors()); 
+app.use(cors());
 
+//Create the new product
+app.post("/create", async (req, res) => {
+  const newProduct = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    discountPercentage: req.body.discountPercentage,
+    rating: req.body.rating,
+    stock: req.body.stock,
+    brand: req.body.brand,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+    images: req.body.images,
+  });
 
+  await Product.create(newProduct);
+  res.send("Product saved to the database!");
+});
+
+//Get the all product list
+app.get("/read", async (req, res) => {
+  const productList = await Product.find();
+  res.send(JSON.stringify(productList));
+});
+
+//Update a product based on the id
+app.put("/update/:id", async (req, res) => {
+    const product_id = req.params.id;
+    await Product.findByIdAndUpdate(product_id, {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      discountPercentage: req.body.discountPercentage,
+      rating: req.body.rating,
+      stock: req.body.stock,
+      brand: req.body.brand,
+      category: req.body.category,
+      thumbnail: req.body.thumbnail,
+      images: req.body.images,
+    });
+  
+    res.send("Product updated successfully!");
+  });
+
+  //Delete a product based on the id
+app.delete("/delete/:id", async (req, res) => {
+    const product_id = req.params.id;
+    await Product.findByIdAndDelete(product_id);
+    res.send("Product deleted!");
+  });
+
+  
 app.listen(port, () => {
-
-console.log(`Server running on port ${port}`);
-
+  console.log(`Server running on port ${port}`);
 });
