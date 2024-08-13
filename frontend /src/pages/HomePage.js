6f1eec0715ce6f8cf1cdfef58798df2781065f85
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-08-13 14:00:20 +0530

    axios have installed

--- frontend /src/pages/HomePage.js
+++ frontend /src/pages/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <React.Fragment>
       <h2>My HomePage</h2>
+      <p>Gayu you r doing well girl. Keep rocking my girl. you can do it. come on cheerup</p>
     </React.Fragment>
   );
 };
